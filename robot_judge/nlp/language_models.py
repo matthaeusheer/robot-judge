89fce738230696e0520971170e05645d3a9aaefc
mathee12 <maheer@student.ethz.ch>
2019-03-25 15:00:07 +0100

    Bundling spacy functionality.

--- robot_judge/nlp/language_models.py
+++ robot_judge/nlp/language_models.py
@@ -3,7 +3,7 @@ import nltk
 from nltk.corpus import stopwords
 
 # Load spacy language model and nltk english stopwords
-spacy_nlp = spacy.load('en')
+spacy_nlp_en = spacy.load('en')
 nltk.download('stopwords')
 stop_words = set(stopwords.words("english"))
 nltk.download('vader_lexicon')
